5dc854392780ba4d5a66165cb78f88442bebfe52
Alexander B <bas2784@gmail.com>
2025-01-16 09:15:48 +0300

    добавление маршрутов на шаблоны

--- UrbanDjango/UrbanDjango/urls.py
+++ UrbanDjango/UrbanDjango/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from task2.views import index
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', index),
+    path('class/', TemplateView.as_view(template_name='second_task/class_template.html')),
 ]
