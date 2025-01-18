e221dc6ed2942180ea09a43999b67c1013a712dc
Alexander B <bas2784@gmail.com>
2025-01-18 18:35:35 +0300

    Сила DTL

--- UrbanDjango/task4/views.py
+++ UrbanDjango/task4/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+data = {
+    'games':
+        ['The Witcher 3: Wild Hunt', 'Grand Theft Auto V', 'The Elder Scrolls V: Skyrim']
+        }
+def shop(request):
+    return render(request, 'fourth_task/shop.html', {'data':data})
