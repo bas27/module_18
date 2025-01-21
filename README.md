07ba4e6378fce8afc34ee21fb9e8effc354d9027
Alexander B. <bas2784@gmail.com>
2025-01-21 16:13:53 +0300

    tpl in django

--- README.md
+++ README.md
@@ -11,7 +11,16 @@
 **_Создание таблиц в базе данных_**\
 `python manage.py migrate`
 
+**_Проверка проекта на ошибки_**\
+`python manage.py check`
+**_Список доступных команд_**\
+`python manage.py help`
 `pip freeze >> requirements.txt`
 
 `django-admin --version`
+
+● Model (Модель): хранит данные.
+● View (Представление): логика обработки запроса.
+● Template (Шаблон): отвечает за отображение данных в удобном формате.
+
 
---
+++ shop/catalog/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from django.http import HttpResponse
+from django.views import View
+
+
+def index(request):
+    products = [
+        {"name": "Смартфон", "price": 15000},
+        {"name": "Ноутбук", "price": 55000},
+        {"name": "Планшет", "price": 25000},
+    ]
+
+    return render(request, 'catalog/index.html', {"products": products})
+
+
+class IndexView(View):
+
+    def get(self, request):
+        return HttpResponse("Добро пожаловать в интернет-магазин!")
+
+    def post(self, request):
+        return HttpResponse("Данные отправлены.")
+
+
+class ProductList(View):
+    def get(self, request):
+        product_items = "<br>".join([f"{p['name']}: {p['price']}руб." for p in products])
+        return HttpResponse(product_items)
