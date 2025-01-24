ae95f17508aa5e762f5fd528f9081b2918e782cd
Alexander B. <bas2784@gmail.com>
2025-01-24 16:24:15 +0300

    Get & post in django

--- shop/catalog/urls.py
+++ shop/catalog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, product_list, add_product
 
 urlpatterns = [
     path('', index, name='index'),
+    path('product_list/', product_list, name='product_list'),
+    path('add_product/', add_product, name='add_product'),
 ]
--- shop/catalog/views.py
+++ shop/catalog/views.py
@@ -1,9 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
 from django.http import HttpResponse
 from django.views import View
+
+products = [
+    {'id': 1, 'name': 'Смартфон Samsung Galaxy', 'category': 'телефоны', 'price': 15000},
+    {'id': 2, 'name': 'Планшет Apple iPad', 'category': 'планшеты', 'price': 45000},
+    {'id': 3, 'name': 'Наушники Sony WH-1000XM4', 'category': 'аксессуары', 'price': 25000},
+    {'id': 4, 'name': 'Ноутбук Dell XPS 13', 'category': 'ноутбуки', 'price': 95000},
+    {'id': 5, 'name': 'Смарт-часы Xiaomi Mi Band', 'category': 'аксессуары', 'price': 3000},
+]
+
+
+def add_product(request):
+    if request.method == 'POST':
+        # Получаем данные из формы
+        name = request.POST.get('name')
+        category = request.POST.get('category')
+        price = request.POST.get('price')
+        # Добавляем новый товар в список
+        product = {'id': len(products) + 1, 'name': name, 'category': category, 'price': price}
+        products.append(product)
+        # Перенаправляем на список товаров
+        return redirect('product_list')  # Имя маршрута списка товаров
+    # Если GET-запрос, отображаем форму
+    return render(request, 'catalog/add_product.html')
+
+def product_list(request):
+    category = request.GET.get('category')
+    if category:
+        filtered_products = [product for product in products if product['category'] == category]
+    else:
+        filtered_products = products
+    return render(request,
+                  'catalog/product_list.html',
+                  {'products': filtered_products})
 
 
 def index(request):
