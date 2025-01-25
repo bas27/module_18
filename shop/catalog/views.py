92ddf16357738762495c90e8111376f1380e89dc
Alexander B. <bas2784@gmail.com>
2025-01-25 11:43:53 +0300

    works with models

--- shop/catalog/views.py
+++ shop/catalog/views.py
@@ -5,13 +5,15 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
 
-products = [
-    {'id': 1, 'name': 'Смартфон Samsung Galaxy', 'category': 'телефоны', 'price': 15000},
-    {'id': 2, 'name': 'Планшет Apple iPad', 'category': 'планшеты', 'price': 45000},
-    {'id': 3, 'name': 'Наушники Sony WH-1000XM4', 'category': 'аксессуары', 'price': 25000},
-    {'id': 4, 'name': 'Ноутбук Dell XPS 13', 'category': 'ноутбуки', 'price': 95000},
-    {'id': 5, 'name': 'Смарт-часы Xiaomi Mi Band', 'category': 'аксессуары', 'price': 3000},
-]
+from .models import Product
+
+# products = [
+#     {'id': 1, 'name': 'Смартфон Samsung Galaxy', 'category': 'телефоны', 'price': 15000},
+#     {'id': 2, 'name': 'Планшет Apple iPad', 'category': 'планшеты', 'price': 45000},
+#     {'id': 3, 'name': 'Наушники Sony WH-1000XM4', 'category': 'аксессуары', 'price': 25000},
+#     {'id': 4, 'name': 'Ноутбук Dell XPS 13', 'category': 'ноутбуки', 'price': 95000},
+#     {'id': 5, 'name': 'Смарт-часы Xiaomi Mi Band', 'category': 'аксессуары', 'price': 3000},
+# ]
 
 
 def add_product(request):
@@ -28,15 +30,20 @@ def add_product(request):
     # Если GET-запрос, отображаем форму
     return render(request, 'catalog/add_product.html')
 
+# def product_list(request):
+#     category = request.GET.get('category')
+#     if category:
+#         filtered_products = [product for product in products if product['category'] == category]
+#     else:
+#         filtered_products = products
+#     return render(request,
+#                   'catalog/product_list.html',
+#                   {'products': filtered_products})
+
+
 def product_list(request):
-    category = request.GET.get('category')
-    if category:
-        filtered_products = [product for product in products if product['category'] == category]
-    else:
-        filtered_products = products
-    return render(request,
-                  'catalog/product_list.html',
-                  {'products': filtered_products})
+    products = Product.objects.all()  # Получаем все товары из базы данных
+    return render(request, 'catalog/product_list.html', {'products': products})
 
 
 def index(request):
@@ -45,7 +52,9 @@ def index(request):
         {"name": "Ноутбук", "price": 55000},
         {"name": "Планшет", "price": 25000},
     ]
-    return render(request, 'catalog/index.html', {"products": products})
+    return render(request,
+                  'catalog/index.html',
+                  {"products": products})
 
 
 class IndexView(View):
